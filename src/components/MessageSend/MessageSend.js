15e03b5d5371ddb0c222dfdce34bfd36e3e5fdfb
reginaldolimas <reginaldolimas@gmail.com>
2023-10-08 20:59:23 -0300

    fix button copy funcional

--- src/components/MessageSend/MessageSend.js
+++ src/components/MessageSend/MessageSend.js
@@ -1,18 +1,27 @@
-        import robo from '../../img/robo.png';
-        import { AiOutlineCopy } from 'react-icons/ai';
+import robo from '../../img/robo.png';
+import { AiOutlineCopy } from 'react-icons/ai';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { useState } from 'react';
 
-        export const MessageSend = ({ message }) => {
-            return (
-                <div class="outgoing-chats">
-                    <div class="outgoing-chats-img">
-                        <img alt='robo' src={robo} />
-                    </div>
-                    <div class="outgoing-msg">
-                        <div class="outgoing-chats-msg">
-                            <p>{message.message}<span className='copyToClipBoard'><AiOutlineCopy /></span></p>
-                            <span class="time">{message.timestamp}</span>
-                        </div>
-                    </div>
+export const MessageSend = ({ message }) => {
+    const [ valueClipBoard, setValueClipBoard ] = useState({value: message.message, copied: false});
+
+    return (
+        <div class="outgoing-chats">
+            <div class="outgoing-chats-img">
+                <img alt='robo' src={robo} />
+            </div>
+            <div class="outgoing-msg">
+                <div class="outgoing-chats-msg">
+                    <p>{message.message}
+                        <CopyToClipboard text={valueClipBoard.value} onCopy={()=>setValueClipBoard({copied: true})}>
+                            <span className='ToClipBoard'><AiOutlineCopy /></span>
+                        </CopyToClipboard>
+                    </p>
+                    <span class="time">{message.timestamp}</span>
+                    {valueClipBoard.copied ? <span style={{color: 'red'}}>Copied.</span> : null}
                 </div>
-            )
-        }
+            </div>
+        </div>
+    )
+}
